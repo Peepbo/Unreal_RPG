e84141db1d5e55e7641dce5a2432e946d4f9e91f
CHAE <tngus10778@gmail.com>
2022-02-27 15:07:12 +0900

    knight animInstance 생성 및 AI 이동을 자연스럽게 수정

---
+++ UnrealRPG/Source/UnrealRPG/KnightAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "KnightAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALRPG_API UKnightAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
